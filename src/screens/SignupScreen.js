31d0cb81f6e933a3b87526de490b0e01ff4f8fb8
syadav <syadav@gozego.com>
2022-08-24 19:13:31 +0530

    Tracks setup and nested navigators

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View,StyleSheet,Text} from 'react-native';
+
+const SignupScreen = () => {
+    return <Text style={{ fontSize: 48}}>Signup Screen</Text>
+
+};
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
---
+++ src/screens/TrackListScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View,StyleSheet,Text} from 'react-native';
+
+const TrackListScreen = () => {
+    return <Text style={{ fontSize: 48}}>TrackList Screen</Text>
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackListScreen;
