c8b8778db5cc7ace27b9553bd392f50095b7e4e0
syadav <syadav@gozego.com>
2022-08-27 12:11:10 +0530

    Using react-native-elements library to create Signup screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import {View,StyleSheet,Text,Button} from 'react-native';
+import {View,StyleSheet} from 'react-native';
+import {Text,Input,Button} from 'react-native-elements';
 
 const SignupScreen = ({ navigation }) => {
     return <>
-       <Text style={{ fontSize: 48}}>Signup Screen</Text>
-       <Button title='Go to Signin' 
-         onPress={ () => navigation.navigate('Signin')} 
-       />
-
-        <Button title='Go to main flow' 
-         onPress={ () => navigation.navigate('mainFlow')} 
-       />
-
+       <Text h3>Sign up for Tracker</Text>
+       <Input label="Email"/>
+       <Input label="Password"/>
+       <Button title="Sign Up"/>  
     </>
-
 };
 
 const styles = StyleSheet.create({});
