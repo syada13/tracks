4fe8816338c0075e1150f6979c909d6956de2ba6
syadav <syadav@gozego.com>
2022-08-29 13:00:52 +0530

    Navigation from outside to react using navigationRef concept

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,7 @@
 import createDataContext from "./createDataContext";
 import trackerApi from '../api/tracker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { navigate } from '../navigationRef';
 
 // Reducer function : authReducer will be called by React directly whenever we call disptach function
 const authReducer = (state, action) => {
@@ -22,6 +23,8 @@ const signup = (disptach) => async({ email, password }) => {
             const response = await trackerApi.post('/signup', { email, password });
             await AsyncStorage.setItem('token', response.data.token);
             disptach({ type: 'signup', payload: response.data.token});
+         
+            navigate('TrackList');
 
            }catch(err){
             disptach({ 
@@ -29,7 +32,7 @@ const signup = (disptach) => async({ email, password }) => {
                 payload: 'Something went wrong during signup!'
             });
            }
-           
+
         // Signup Success: Modify our state, and say that we're authenticated
 
         // Signup failure: Reflect an error message
