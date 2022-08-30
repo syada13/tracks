8595d151580e211a11864fd2aae9ce826b1d4ec6
syadav <syadav@gozego.com>
2022-08-30 23:46:41 +0530

    Create a reusable auth form for Signup and Sigin screens

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,45 +1,22 @@
-import React ,{useState,useContext} from 'react';
+import React ,{useContext} from 'react';
 import {View,StyleSheet,TouchableOpacity} from 'react-native';
-import {Text,Input,Button} from 'react-native-elements';
+import {Text} from 'react-native-elements';
 import Spacer from './components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
-
+import AuthFrom from './components/AuthForm';
 
 const SignupScreen = ({ navigation }) => {
-
    const {state, signup} = useContext(AuthContext);
-   const [email,setEmail] = useState('');
-   const [password,setPassword] = useState('');
-
+   
     return (
     <View style={styles.container}>
-      <Spacer>
-         <Text h3>Sign up for Tracker</Text>
-      </Spacer>
-       <Input 
-         label="Email"
-         value={email}
-         onChangeText={setEmail}
-         autoCapitalize="none"
-         autoCorrect={false}
-        />
-       <Spacer/>
-       <Input 
-         secureTextEntry
-         label="Password"
-         value={password}
-         onChangeText={setPassword}
-         autoCapitalize="none"
-         autoCorrect={false}
-        />
-        {state.errorMessage ? (
-        <Text style={styles.errorMessage}>{state.errorMessage}</Text> 
-        ): null}
-        <Spacer>
-         <Button 
-          title="Sign Up" 
-          onPress={() => signup({ email,password })}/> 
-        </Spacer> 
+      <AuthFrom
+        headerText="Sign Up for Tracker"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign Up"
+        onSubmit={signup}
+      />
+      
         <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
           <Spacer>
           <Text style={styles.link}>
@@ -62,11 +39,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginBottom:250
-  },
-  errorMessage:{
-    fontSize: 16,
-    color:'red',
-    marginLeft:15
   },
   link:{
     color:'blue'
