58456b505db787a0b127939e71231ee0437ce4c5
syadav <syadav@gozego.com>
2022-09-15 12:23:44 +0530

    Live location updates

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://1183-2409-4050-e3a-dbde-89a-59d1-ad62-b233.ngrok.io'
+    baseURL: 'http://0afb-2409-4050-2e05-4466-75b5-8f57-1539-ac80.ngrok.io'
 });
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,18 +1,28 @@
 import '../_mockLocation';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,useContext } from 'react';
 import {StyleSheet} from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView} from 'react-navigation';
 import Map from '../components/Map';
-import { requestForegroundPermissionsAsync} from 'expo-location';
+import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackCreateScreen = () => {
-
+    const { addLocation } = useContext(LocationContext);
     const [err, setErr] = useState(null);
+    
 
     const startWatching = async () => {
         try {
-           const {granted} = await requestForegroundPermissionsAsync();
+           await requestForegroundPermissionsAsync();
+           await watchPositionAsync({
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10
+           }, (location) => {
+            addLocation(location);
+
+           });
 
         }catch (e){
             setErr(e);
