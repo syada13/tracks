c735adea29195b367043d19340d93a6dd46deef0
syadav <syadav@gozego.com>
2022-09-21 19:12:24 +0530

    Wire up the track form

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React ,{ useContext }from 'react';
 import {Input, Button} from 'react-native-elements';
 import Spacer from './Spacer';
+import {Context as LocationContext} from '../context/LocationContext';
 
 const TrackForm = () => {
+
+   const { state: {name, recording}, 
+      startRecording, 
+      stopRecording, 
+      changeName
+    } = useContext(LocationContext);
+
     return (
     <>
         <Spacer>
-         <Input placeholder="Enter name"/>
+         <Input value={name} onChangeText={changeName} placeholder="Enter name"/>
         </Spacer>
-        <Button  title="Start Recording" />
-     
+        { recording 
+          ? <Button  title="Stop" onPress={stopRecording} />
+          : <Button  title="Start Recording"  onPress={startRecording} />        
+        }   
     </>
     );
 
